415ddcb34e175a9220c103a064f250e2a4b7f832
Matthew Hagan <matthewhagan15@gmail.com>
2017-11-21 22:57:54 -0500

    Added 2012 data and started new directory

---
+++ 2012/formatResults.py
@@ -0,0 +1,75 @@
+import numpy as np 
+import pandas as pd
+
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'District of Columbia':'DC',
+    'Wyoming': 'WY',
+}
+
+d = {}
+
+fname = './US_elect_county.csv'
+
+data = pd.read_csv(fname)
+data = data[['State','County','Percent GOP']]
+
+rows = []
+count = 0
+for index,row in data.iterrows():
+	state,county = row['State'],row['County']
+	if state == us_state_abbrev.get(county,None) and not d.get(state,False):
+		d[state] = True
+		rows.append(index)
+
+for index in rows:
+	data.drop(index,inplace=True)
+data.to_csv('./modified_elec_results.csv')
