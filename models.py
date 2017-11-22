aa69cba2e824d1e0df9ca8e75f862a52c1c826a6
Frank Soucy <franksoucy9@gmail.com>
2017-11-21 23:04:03 -0500

    Eliminating features using LASSO.

--- models.py
+++ models.py
@@ -6,14 +6,20 @@ import pdb
 import get_y 
 import sklearn.linear_model
 from sklearn import preprocessing
+from sklearn.linear_model import Lasso
 import combineData as cd 
+import copy
 
 
 
 
-folder = './no_edu_data/'
+folder = './data/'
 
-X = cd.loadFilesFrom(folder)
+#X = cd.loadFilesFrom(folder)
+X = pd.read_csv('./data/education.csv',encoding='mac_roman')
+X = X.set_index("Geography")
+cols = X.columns
+originalData = copy.deepcopy(X)
 y = pd.read_csv('./election_results.csv')
 X = cd.addYcol(X,y)
 
@@ -27,17 +33,96 @@ for i in range(len(mat)):
         except:
             mat[i][j] = 0.0
 mat = np.array(mat)
-np.random.shuffle(mat)
+#np.random.shuffle(mat)
 X = mat[:,:-1]
+Y = mat[:,-1]
+#Y = Y * 10000.0
+
 X = X.T
 np.random.shuffle(X)
 X = X.T
-if (X.shape[1] > 180):
-    X = X[:,:180]
+
+if (X.shape[1] > 93):
+    X = X[:,:93]
+"""
+
+#def determineEpsLam(X, Y, start, batchSize):
+#    pass
+
+def makeX(X, start, batchSize):
+    currentX = X[:, start:start+batchSize]
+    return currentX
+
+def lassoDict(currentX, currentY, eps, lam, currentColumns):
+    irrelevant = []
+    model = Lasso(alpha=lam, fit_intercept=True)
+    model.fit(currentX, currentY)
+    params = model.get_params()
+    print(model.coef_.sum())
+    for i in range(model.coef_.shape[0]):
+        if np.abs(model.coef_[i]) < eps:
+            irrelevant.append(currentColumns[i])
+    return irrelevant
+
+def updateIrrelevant(lasso_result, irrelevantColumns):
+    for feature in lasso_result:
+        if feature in irrelevantColumns:
+            irrelevantColumns[feature] += 1
+        else:
+            irrelevantColumns[feature] = 1
+
+def outputNewFeatures(doc_name, originalData, irrelevantColumns, threshold, ind):
+    print("Originally had " + str(len(originalData.columns)) + " features.")
+    for feature in irrelevantColumns:
+        if irrelevantColumns[feature] > threshold and feature != ind:
+            originalData = originalData.drop(feature, 1)
+    print("Now have " + str(len(originalData.columns)) + " features.")
+    originalData.to_csv(doc_name)
+    return originalData
+
+irrelevantColumns = {}
+
+# eps, lam = determineEpsLam(X, Y, start, batchSize)
+eps = 100.0
+lam = 100.0
+start = 0
+batchSize = 5
+stride = 1
+threshold = 3
+
+# makeX, makeY
+while (start+batchSize) < len(cols):
+    currentX = makeX(X, start, batchSize)
+    currentColumns = cols[start: start+batchSize]
+    lasso_result = lassoDict(currentX, Y, eps, lam, currentColumns)
+    updateIrrelevant(lasso_result, irrelevantColumns)
+    start += stride
+
+doc_name = "edu_test.csv"
+
+originalData = outputNewFeatures(doc_name, originalData, irrelevantColumns, threshold, "Geography")
+
+
+mat = originalData.as_matrix()
+mat = list(mat)
+for i in range(len(mat)):
+    for j in range(len(mat[i])):
+        val = mat[i][j]
+        try:
+            mat[i][j] = float(val)
+        except:
+            mat[i][j] = 0.0
+mat = np.array(mat)
+X = mat
+Y = Y / 10000.0
+"""
+
+
 print("Num features: " + str(X.shape[1]))
-Y = mat[:,-1]
 trainX = X[:600,:]; trainY = Y[:600];
 testX = X[600:, :]; testY = Y[600:];
+
+print(trainX.shape)
 
 
 model = sklearn.linear_model.LinearRegression(fit_intercept=True)
